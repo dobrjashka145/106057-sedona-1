6e7102c9ad2d69a757200cb84b5518e158a54c4e
dobrjashka145 <dobrjashka145@gmail.com>
2016-08-31 07:03:01 +0300

    Исправлено модальное окно modal-hotels
    Исправлена анимация ощибки ввода

--- js/popup-hotels.js
+++ js/popup-hotels.js
@@ -2,15 +2,18 @@
   var popup = document.querySelector(".modal-hotels-form")
   var dateEntry = popup.querySelector("[name=entry-date]")
   var dateExit = popup.querySelector("[name=exit-date]")
+  var btnSubmit = popup.querySelector(".btn-submit")
   var form = popup.querySelector("form")
   
   headerForm.addEventListener("click", function(event) {
     event.preventDefault();
 	if (popup.classList.contains("modal-show")) {
-	  popup.classList.remove("modal-show");
+	    popup.classList.remove("modal-show");
+		dateEntry.classList.remove("modal-error");
+	    dateExit.classList.remove("modal-error");
 	} else {
 		popup.classList.add("modal-show");
-		EntryDate.focus();
+		dateEntry.focus();
 	}
 	});
     
@@ -18,13 +21,25 @@
     if (event.keyCode === 27) {
      if (popup.classList.contains("modal-show")) {
        popup.classList.remove("modal-show");
+	   dateEntry.classList.remove("modal-error");
+	   dateExit.classList.remove("modal-error");
      }
     }
   });
   
-  form.addEventListener("submit", function(event) {
-    if (!dateEntry.value) {
+  btnSubmit.addEventListener("click", function(event) {
+    if (dateEntry.classList.contains("modal-error")) {
+        dateEntry.classList.remove("modal-error");
+	}
+	if (dateExit.classList.contains("modal-error")) {
+        dateExit.classList.remove("modal-error");
+	}
+	if (!dateEntry.value) {
       event.preventDefault();
       dateEntry.classList.add("modal-error");
-    } 
+	}
+    if (!dateExit.value) {
+      event.preventDefault();
+      dateExit.classList.add("modal-error");
+	} 	
   });
